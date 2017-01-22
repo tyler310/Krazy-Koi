e0b679ed9fb3e8718e81e0b2258c5f20e3abb934
Tyler Young <tyler0310@gmail.com>
2017-01-22 01:45:45 -0800

    Working audio engine with Todd, not yet synced with actual game

---
+++ Assets/Scripts/ComposerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComposerScript : MonoBehaviour
+{
+
+    public int bpm;
+    public float crotchet;
+    public float lastBeat;
+    public float offset = 0.2f;
+    public float songPosition;
+
+    private float lastTime, deltaTime, timer;
+
+    public GameObject orb;
+    public Transform orbSpawn;
+    public AudioSource song;
+
+    // Use this for initialization
+    void Start()
+    {
+        lastBeat = 0;
+        crotchet = 60/bpm;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+//        if (songPosition > lastBeat + crotchet)
+//        {
+            //            songPosition = (float)(AudioSettings.dspTime – dsptimesong) * song.pitch – offset;
+            //
+            //            Instantiate(orb, orbSpawn);
+            //	        lastBeat += crotchet;
+
+            deltaTime = GetComponent<AudioSource>().time - lastTime;
+            timer += deltaTime;
+
+            if (timer > lastBeat + crotchet)
+            {
+                //Create the note
+                ((GameObject) Instantiate(orb, orbSpawn)).GetComponent<Transform>().parent = GetComponent<Transform>();
+                timer -= (60f/bpm);
+                lastBeat += crotchet;
+            }
+//        }
+    }
+} 
---
+++ Assets/Scripts/OrbScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SynchronizerData;
+using UnityEngine;
+
+public class OrbScript : MonoBehaviour {
+
+    public Vector3[] beatPositions;
+    private BeatObserver beatObserver;
+    private int beatCounter;
+
+    // Use this for initialization
+    void Start ()
+    {
+        beatObserver = GetComponent<BeatObserver>();
+        beatCounter = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Translate(Vector2.right * speed * Time.deltaTime);
+        if ((beatObserver.beatMask & BeatType.OnBeat) == BeatType.OnBeat)
+        {
+            transform.position = beatPositions[beatCounter];
+            beatCounter = (++beatCounter == beatPositions.Length ? 0 : beatCounter);
+        }
+
+    }
+}
